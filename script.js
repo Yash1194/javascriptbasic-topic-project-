0eef991d8db8b01b3ba2427bf3e4a286e9b09d2b
Yash Swarnkar <ys703981@gmail.com>
2025-11-14 13:15:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = document.querySelector("#bulb")
+var btn = document.querySelector("button")
+var flag = 0
+btn.addEventListener("click", function(){
+    if(flag ==0){bulb.style.backgroundColor = "yellow"
+    console.log("cliocked")
+flag = 1}else{
+    bulb.style.backgroundColor = "transparent"
+    console.log("cliocked")
+    flag = 0
+}
+})
